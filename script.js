6cd460ce0037817ef40fa1af4158ad1e1547d437
Rafael <codebuilder.js@gmail.com>
2023-12-08 19:56:22 -0300

    Set up a hover effect on each grid divs

--- script.js
+++ script.js
@@ -2,9 +2,18 @@ const container = document.querySelector('.container');
 
 for(let i = 1; i <= 256; i++) {
     const gridItem = document.createElement('div');
-
+    
     gridItem.style.border = '1px solid black';
     gridItem.style.width = '16px';
     gridItem.style.height = '16px';
+
+    gridItem.classList.add('grid-background-color');
+    
     container.appendChild(gridItem);
 }
+
+const grid = document.querySelectorAll('.grid-background-color');
+
+grid.forEach(target => target.addEventListener('mouseover', () => {
+    target.style.backgroundColor = 'black';
+}));
