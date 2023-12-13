c5de147b1efeed390ce3cbb1c3b9b674886d254d
Rafael <codebuilder.js@gmail.com>
2023-12-12 21:06:55 -0300

    Add a slider that changes the grid's width

--- script.js
+++ script.js
@@ -83,6 +83,7 @@ createGrid();
 paintBox();*/
 
 const container = document.querySelector('.container');
+const slider = document.querySelector('#grid-range');
 
 function createGrid(size) {
     const maxWidth = 600;
@@ -102,4 +103,20 @@ function createGrid(size) {
     }
 }
 
-createGrid(10);
+function paint() {
+    const gridSquares = document.querySelectorAll('.paint');
+
+    gridSquares.forEach(target => target.addEventListener('mouseover', () => {
+        target.style.backgroundColor = 'black';
+    }));
+}
+
+slider.addEventListener('input', (e) => {
+    container.innerHTML = "";
+
+    createGrid(e.target.value);
+    paint();
+});
+
+createGrid(slider.value);
+paint();
